81b87285d01d6760893ee164dd0a3fec8a4bfa25
Liu-Curiousity <2675794963@qq.com>
2026-04-28 01:23:13 +0800

    移植LetterShell

---
+++ App/QGimbal_config.h
@@ -0,0 +1,32 @@
+/**
+ * @file        QGimbal_config.h
+ * @brief 		用于定义QGimbal的配置常量
+ * @detail
+ * @author      Liu-Curiousity (2675794963@qq.com)
+ * @date        26-4-28
+ * @version 	V1.0.0
+ * @note 		
+ * @warning	    
+ * @par 		历史版本
+                V1.0.0创建于26-4-28
+ * @copyright   (c) 2026 QDrive
+ * */
+
+#ifndef QGIMBAL_CONFIG_H
+#define QGIMBAL_CONFIG_H
+
+#define QGIMBAL_SOFTWARE_VERSION "2.0.0"
+
+/*==========================云台参数==========================*/
+
+/*==========================配置参数==========================*/
+#define QGIMBAL_MAX_SPEED           1000.0f   // 最大转速,单位rpm
+
+#define QGIMBAL_SPEED_KP            3e-3f
+#define QGIMBAL_SPEED_KI            7.8e-5f
+#define QGIMBAL_SPEED_KD            0.0f
+#define QGIMBAL_ANGLE_KP            1200.0f
+#define QGIMBAL_ANGLE_KI            0.0f
+#define QGIMBAL_ANGLE_KD            0.0f
+
+#endif //QGIMBAL_CONFIG_H
