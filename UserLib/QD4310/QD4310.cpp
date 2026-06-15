2361dcc46d3c3f42eea9efd9f9f93982d83f5010
Liu-Curiousity <2675794963@qq.com>
2026-06-15 21:36:29 +0800

    1.更新QD4310驱动库

--- UserLib/QD4310/QD4310.cpp
+++ UserLib/QD4310/QD4310.cpp
@@ -1,15 +1,16 @@
 /**
- * @file        QD4310.cpp
+ * @file        QD4310.h
  * @brief       基于HAL库的QD4310电机CAN总线控制库
  * @details
  * @author      Liu-Curiousity (2675794963@qq.com)
- * @date        2026-4-28
- * @version     V1.1.1
+ * @date        2026-6-15
+ * @version     V1.2.0
  * @note
  * @warning
  * @par         历史版本:
  *		        V1.1.0创建于2026-1-25
  *		        V1.1.1创建于2026-4-28, 修复clamp限幅失效的问题
+ *		        V1.2.0创建于2026-6-15, 添加零点设置和重启设备
  * @copyright   (c) 2026 QDrive
  */
 
@@ -46,26 +47,26 @@ void QD4310::nop() const {
 
 void QD4310::setAngle(float _angle) const {
     _angle = std::clamp(_angle, 0.0f, 2 * std::numbers::pi_v<float>); // 限制角度在[0, 2pi]范围内
-    SendCommand(Command::ANGLE, _angle / 2 / std::numbers::pi_v<float> * UINT16_MAX);
+    SendCommand(Command::AngleCtrl, _angle / 2 / std::numbers::pi_v<float> * UINT16_MAX);
 }
 
-void QD4310::setStepAngle(float _step_angle) const {
+void QD4310::setStepAngle(float step_angle_) const {
     // 限制角度在[-2pi, 2pi]范围内
-    _step_angle = std::clamp(_step_angle, -2 * std::numbers::pi_v<float>, 2 * std::numbers::pi_v<float>);
-    SendCommand(Command::STEP_ANGLE, _step_angle / 2 / std::numbers::pi_v<float> * INT16_MAX);
+    step_angle_ = std::clamp(step_angle_, -2 * std::numbers::pi_v<float>, 2 * std::numbers::pi_v<float>);
+    SendCommand(Command::StepAngleCtrl, step_angle_ / 2 / std::numbers::pi_v<float> * INT16_MAX);
 }
 
-void QD4310::setSpeed(float _speed) const {
-    _speed = std::clamp(_speed, -1000.0f, 1000.0f); // 限制速度在[-1000, 1000]范围内
-    SendCommand(Command::SPEED, _speed / 1000 * INT16_MAX);
+void QD4310::setSpeed(float speed_) const {
+    speed_ = std::clamp(speed_, -1000.0f, 1000.0f); // 限制速度在[-1000, 1000]范围内
+    SendCommand(Command::SpeedCtrl, speed_ / 1000 * INT16_MAX);
 }
 
-void QD4310::setLowSpeed(float _speed) const {
-    _speed = std::clamp(_speed, -1000.0f, 1000.0f); // 限制速度在[-1000, 1000]范围内
-    SendCommand(Command::LOW_SPEED, _speed / 1000 * INT16_MAX);
+void QD4310::setLowSpeed(float speed_) const {
+    speed_ = std::clamp(speed_, -1000.0f, 1000.0f); // 限制速度在[-1000, 1000]范围内
+    SendCommand(Command::LowSpeedCtrl, speed_ / 1000 * INT16_MAX);
 }
 
-void QD4310::setCurrent(float _current) const {
-    _current = std::clamp(_current, -10.0f, 10.0f);
-    SendCommand(Command::CURRENT, _current / 10 * INT16_MAX);
+void QD4310::setCurrent(float current_) const {
+    current_ = std::clamp(current_, -10.0f, 10.0f);
+    SendCommand(Command::CurrentCtrl, current_ / 10 * INT16_MAX);
 }
--- UserLib/QD4310/QD4310.h
+++ UserLib/QD4310/QD4310.h
@@ -3,13 +3,14 @@
  * @brief       基于HAL库的QD4310电机CAN总线控制库
  * @details
  * @author      Liu-Curiousity (2675794963@qq.com)
- * @date        2026-4-28
- * @version     V1.1.1
+ * @date        2026-6-15
+ * @version     V1.2.0
  * @note
  * @warning
  * @par         历史版本:
  *		        V1.1.0创建于2026-1-25
  *		        V1.1.1创建于2026-4-28, 修复clamp限幅失效的问题
+ *		        V1.2.0创建于2026-6-15, 添加零点设置和重启设备
  * @copyright   (c) 2026 QDrive
  */
 
@@ -24,8 +25,10 @@ public:
     explicit QD4310(CAN_HandleTypeDef *hcan, const uint8_t id) :
         id(id), hcan(hcan) {}
 
-    void enable() const { SendCommand(Command::ENABLE, 0x0000); }
-    void disable() const { SendCommand(Command::DISABLE, 0x0000); }
+    void enable() const { SendCommand(Command::Enable, 0x0000); }
+    void disable() const { SendCommand(Command::Disable, 0x0000); }
+    void reboot() const { SendCommand(Command::Reboot, 0x0000); }
+    void setZeroPos() const { SendCommand(Command::SetZeroPos, 0x0000); }
     void update(const uint8_t feedback[8]);
 
     void nop() const;
@@ -36,24 +39,24 @@ public:
     void setAngle(float _angle) const;
     /**
      * @brief 设置电机角度
-     * @param _step_angle 设置的角度,[-2pi,2pi]
+     * @param step_angle_ 设置的角度,[-2pi,2pi]
      */
-    void setStepAngle(float _step_angle) const;
+    void setStepAngle(float step_angle_) const;
     /**
      * @brief 设置电机转速
-     * @param _speed 设置的转速,[-1000,1000]
+     * @param speed_ 设置的转速,[-1000,1000]
      */
-    void setSpeed(float _speed) const;
+    void setSpeed(float speed_) const;
     /**
      * @brief 设置电机转速
-     * @param _speed 设置的转速,[-1000,1000]
+     * @param speed_ 设置的转速,[-1000,1000]
      */
-    void setLowSpeed(float _speed) const;
+    void setLowSpeed(float speed_) const;
     /**
      * @brief 设置电机电流
-     * @param _current 设置的转速,[-10,10]
+     * @param current_ 设置的转速,[-10,10]
      */
-    void setCurrent(float _current) const;
+    void setCurrent(float current_) const;
 
     bool enabled{};
     uint8_t id;      // CAN id
@@ -62,14 +65,18 @@ public:
     float current{}; // in A
 private:
     enum class Command :uint8_t {
-        NOP = 0x00,
-        ENABLE = 0x01,
-        DISABLE = 0x02,
-        CURRENT = 0x03,
-        SPEED = 0x04,
-        ANGLE = 0x05,
-        LOW_SPEED = 0x06,
-        STEP_ANGLE = 0x07
+        NOP = 0x00,           // 无操作
+        Enable = 0x01,        // 使能
+        Disable = 0x02,       // 失能
+        CurrentCtrl = 0x03,   // 电流控制
+        SpeedCtrl = 0x04,     // 速度控制
+        AngleCtrl = 0x05,     // 角度控制
+        LowSpeedCtrl = 0x06,  // 低速控制
+        StepAngleCtrl = 0x07, // 角度步进控制
+
+        Reboot = 0xFF,     // 重启
+        SetZeroPos = 0xFE, // 设置零点
+        ClearError = 0xFB, // 清除错误
     };
 
     CAN_HandleTypeDef *hcan{};
